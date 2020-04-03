64d243267b332d4e9564a1a12b6181d4188cee80
bhaskarrc <bhaskaroychowdhury@gmail.com>
2020-04-04 03:24:17 +0530

    Changing pom.xml

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -1,7 +1,9 @@
 package guru.springframework.spring5webapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class Spring5webappApplication {
